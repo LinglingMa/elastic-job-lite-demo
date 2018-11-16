89b814739811353791ad2b9b5a262df2a40afd89
malingling <malingling@kuaishou.com>
2018-11-16 16:30:48 +0800

    add elastic job spring boot starter

--- elastic-job-springboot-starter/src/main/java/io/elasticjob/lite/springboot/App.java
+++
@@ -1,8 +0,0 @@
-package io.elasticjob.lite.springboot;
-
-/**
- * @author malingling <malingling@kuaishou.com>
- * Created on 2018-11-16
- */
-public class App {
-}
--- elastic-job-springboot-starter/src/main/java/io/elasticjob/lite/springboot/autoconfigure/JobParserAutoConfiguration.java
+++ elastic-job-springboot-starter/src/main/java/io/elasticjob/lite/springboot/autoconfigure/JobParserAutoConfiguration.java
@@ -1,6 +1,11 @@
 package io.elasticjob.lite.springboot.autoconfigure;
 
+import io.elasticjob.lite.reg.zookeeper.ZookeeperConfiguration;
+import io.elasticjob.lite.reg.zookeeper.ZookeeperRegistryCenter;
+import io.elasticjob.lite.springboot.parser.JobConfParser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -9,6 +14,27 @@ import org.springframework.context.annotation.Configuration;
  */
 
 @Configuration
-@EnableConfigurationProperties()
+@EnableConfigurationProperties(ZookeeperProperties.class)
 public class JobParserAutoConfiguration {
+
+    @Autowired
+    private ZookeeperProperties zookeeperProperties;
+
+    @Bean(initMethod = "init")
+    public ZookeeperRegistryCenter zookeeperRegistryCenter() {
+        ZookeeperConfiguration zkConfig = new ZookeeperConfiguration(zookeeperProperties.getServerLists(),
+                zookeeperProperties.getNamespace());
+        zkConfig.setBaseSleepTimeMilliseconds(zookeeperProperties.getBaseSleepTimeMilliseconds());
+        zkConfig.setConnectionTimeoutMilliseconds(zookeeperProperties.getConnectionTimeoutMilliseconds());
+        zkConfig.setDigest(zookeeperProperties.getDigest());
+        zkConfig.setMaxRetries(zookeeperProperties.getMaxRetries());
+        zkConfig.setMaxSleepTimeMilliseconds(zookeeperProperties.getMaxSleepTimeMilliseconds());
+        zkConfig.setSessionTimeoutMilliseconds(zookeeperProperties.getSessionTimeoutMilliseconds());
+        return new ZookeeperRegistryCenter(zkConfig);
+    }
+
+    @Bean
+    public JobConfParser jobConfParser() {
+        return new JobConfParser();
+    }
 }
